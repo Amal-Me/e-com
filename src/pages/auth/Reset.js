ac900283ae81d6b2e29555aa5f8de5562c0ea603
Amal <83458917+Amal-Me@users.noreply.github.com>
2022-10-06 20:19:10 +0200

    reset password

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./auth.module.scss";
 import resetImg from "../../assets/forgot.png";
 import Card from '../../components/card/Card';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { auth } from '../../firebase/config';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import Loader from '../../components/loader/Loader';
 
 const Reset = () => {
+
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const resetPassword = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+  sendPasswordResetEmail(auth, email)
+  .then(() => {
+      toast.success("check your email for a reset link");
+      setIsLoading(false);
+    })
+    .catch((error) => {
+      toast.error(error.message);
+      setIsLoading(false);
+    });
+  }
+
   return (
+    <>
+    {isLoading && <Loader />}
     <section className={`container ${styles.auth}`}> 
         <div className={styles.img}>
             <img src={resetImg} alt="Reset password" width={400}/>
@@ -13,9 +38,13 @@ const Reset = () => {
         <Card>
         <div className={styles.form}>
             <h2>Reset Password</h2>            
-            <form>
-                <input type="text" placeholder='Email' required/>               
-                <button className='--btn --btn-primary --btn-block'>Reset Password</button>
+            <form onSubmit={resetPassword}>
+                <input type="text" placeholder='Email' required 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}/>               
+                <button type='submit' className='--btn --btn-primary --btn-block'>
+                  Reset Password
+                  </button>
                 <div className={styles.links}>
                   <p>
                   <Link to="/login">- Login</Link>                  
@@ -28,6 +57,7 @@ const Reset = () => {
         </div>
         </Card>        
     </section>
+    </>
   )
 }
 
