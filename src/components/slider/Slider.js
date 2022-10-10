f938a3a5f7aada4c299e2873b484fa457cb9575a
Amal <83458917+Amal-Me@users.noreply.github.com>
2022-10-10 17:42:24 +0200

    slide

---
+++ src/components/slider/Slider.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import "./Slider.scss";
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai";
+import { sliderData } from './slider-data';
+
+const Slider = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slideLength = sliderData.length;
+
+    const nextSlide = () => {
+        // si c'est la derniere img on revient a 0 sinon + 1
+        setCurrentSlide(currentSlide === slideLength -1 ? 0 : currentSlide + 1);
+    };
+    const prevSlide = () => {
+        // si premierez img on revient a la derniere - 1
+        setCurrentSlide(currentSlide === 0  ? slideLength -1 : currentSlide - 1);
+    };
+
+  return (
+    <div className='slider'>
+      <AiOutlineArrowLeft className='arrow prev' onClick={prevSlide}/>
+      <AiOutlineArrowRight className='arrow next' onClick={nextSlide}/>
+
+      {sliderData.map((slide, index) => {
+        const {image, heading, desc} = slide
+        return(
+           <div key={index}className={index === currentSlide ? "slide current"  : "slide"}>
+            {index === currentSlide && (
+                <>
+                <img src={image} alt="slide" />
+                <div className="content">
+                    <h2>{heading}</h2>
+                    <p>{desc}</p>
+                    <hr />
+                    <a href="#product" className='--btn --btn-primary'>
+                        Shop Now
+                    </a>
+                </div>
+                </>
+            )}
+
+           </div>
+        )
+      })}
+    </div>
+  );
+};
+
+export default Slider;
