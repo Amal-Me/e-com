526e629774a61f1152d3fc614140cb723f2f1fec
Amal <83458917+Amal-Me@users.noreply.github.com>
2022-10-14 14:26:16 +0200

    display products admin

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,8 +1,10 @@
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { db } from '../../../firebase/config';
 import styles from "./ViewProducts.module.scss";
+import {FaEdit, FaTrashAlt} from "react-icons/fa";
 
 const ViewProducts = () => {
   const [products, setProducts] = useState([]);
@@ -31,6 +33,7 @@ const ViewProducts = () => {
           }));
           console.log(allProducts);
           setProducts(allProducts);
+          setIsLoading(false);
         });   
       
       } catch (error) {
@@ -39,9 +42,60 @@ const ViewProducts = () => {
       }
   }
   return (
-    <div>
-      <h2>vp</h2>
+    <>
+    <div className={styles.table}>
+    <h2>All Products</h2>
+
+    {products.length === 0 ? (
+    <p>No products found.</p>) : (
+      <table>
+        <thead>
+        <tr>
+          <th>s/n</th>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Price</th>
+          <th>Actions</th>          
+        </tr>
+        </thead>
+        <tbody>
+        {products.map((product, index) => {
+          const {id, name, price, imageURL, category} = product;
+          return(            
+            <tr key={id}>
+              <td>
+                {index + 1}
+              </td>
+              <td>
+                <img src={imageURL} alt={name}  style={{width: "100px"}}/>
+              </td>
+              <td>
+                {name}
+              </td>
+              <td>
+                {category}
+              </td>
+              <td>
+                {`$${price}`}
+              </td>
+              <td>
+                <Link to="/admin/add-product">
+                  <FaEdit size={20} color="green"/>
+                </Link>
+                &nbsp;
+                <FaTrashAlt size={18} color="red"/>
+              </td>
+            </tr>
+            
+          )
+        })}
+        </tbody>
+      </table>
+    )}
     </div>
+      
+    </>
   )
 };
 
